84b84e8406d9561fcfd872d0c3887789ff768056
Brandon Lim <brandonlim.lim@gmail.com>
2020-01-26 23:49:45 +1100

    Refactor stix fonts resources to fonts/stix_fonts directory

--- build.sh
+++ build.sh
@@ -1,13 +1,51 @@
 #!/bin/bash
 
+usage() {
+    cat <<EOF
+Usage: $(basename "$0") [OPTIONS]
+
+Options:
+  -h, --help            Show usage
+  -t, --target string   Directory that contains files to build layer zip
+EOF
+}
+
+if [ "$#" -eq 0 ]; then
+    usage
+    exit 1
+fi
+
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+    -h | --help)
+        usage
+        exit 0
+        ;;
+    -t | --target)
+        shift
+        TARGET=$1
+        ;;
+    *)
+        usage
+        exit 1
+        ;;
+    esac
+    shift
+done
+
+if [ ! -f "$TARGET/Dockerfile" ]; then
+    echo "No dockerfile found in directory $TARGET"
+    exit 1
+fi
+
 set -e
 
-DOCKER_IMAGE_TAG="fonts:amazonlinux"
+DOCKER_IMAGE_TAG="amazonlinuxfonts:$(basename "$TARGET")"
 LAYER_ZIP="layer.zip"
 
-rm -f $LAYER_ZIP 1>/dev/null
+rm -f "$TARGET/$LAYER_ZIP" 1>/dev/null
 
-docker build -t $DOCKER_IMAGE_TAG .
+docker build -t $DOCKER_IMAGE_TAG $TARGET
 CONTAINER=$(docker run -d $DOCKER_IMAGE_TAG)
-docker cp $CONTAINER:/$LAYER_ZIP $LAYER_ZIP
+docker cp $CONTAINER:/$LAYER_ZIP "$TARGET/$LAYER_ZIP"
 docker rm -f $CONTAINER
--- publish.sh
+++ publish.sh
@@ -1,13 +1,61 @@
 #!/bin/bash
 
-ARN_DIR="arns"
-ARN_FILE="$ARN_DIR/stix-fonts.csv"
-LAYER_NAME="stix-fonts"
-LAYER_ZIP="layer.zip"
+usage() {
+    cat <<EOF
+Usage: $(basename "$0") [OPTIONS]
+
+Options:
+  -d, --description string  Description of the layer
+  -h, --help                Show usage
+  -t, --target string       Directory that contains layer zip
+EOF
+}
+
+if [ "$#" -eq 0 ]; then
+    usage
+    exit 1
+fi
+
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+    -d | --description)
+        shift
+        LAYER_DESCRIPTION=$1
+        ;;
+    -h | --help)
+        usage
+        exit 0
+        ;;
+    -t | --target)
+        shift
+        TARGET=$1
+        ;;
+    *)
+        usage
+        exit 1
+        ;;
+    esac
+    shift
+done
+
+ARN_DIR="$TARGET/arns"
+ARN_FILE="$ARN_DIR/$(basename "$TARGET").csv"
+LAYER_NAME=$(basename "$TARGET")
+LAYER_ZIP="$TARGET/layer.zip"
 REGIONS=$(cat config/regions.txt)
 
-# Exit if aws cli is not available
+if [ ! -f "$LAYER_ZIP" ]; then
+    echo "No layer zip found in directory $TARGET"
+    exit 1
+fi
+
+if [ -z "$LAYER_DESCRIPTION" ]; then
+    echo "No layer description provided"
+    exit 1
+fi
+
 if ! aws --version 1>/dev/null; then
+    echo "aws command not found"
     exit 1
 fi
 
@@ -18,18 +66,18 @@ for region in $REGIONS; do
     printf "%s\n" "Region: $region"
     OUTPUT=$(
         aws lambda publish-layer-version \
-            --description "stix-fonts" \
-            --layer-name $LAYER_NAME \
+            --description "$LAYER_DESCRIPTION" \
+            --layer-name "$LAYER_NAME" \
             --output text \
             --query "[LayerVersionArn, Version]" \
-            --region $region \
-            --zip-file fileb://$LAYER_ZIP
+            --region "$region" \
+            --zip-file "fileb://$LAYER_ZIP"
     )
     LAYER_VERSION_ARN=$(echo $OUTPUT | awk '{print $1}')
     LAYER_VERSION=$(echo $OUTPUT | awk '{print $2}')
     aws lambda add-layer-version-permission \
         --action lambda:GetLayerVersion \
-        --layer-name $LAYER_NAME \
+        --layer-name "$LAYER_NAME" \
         --output text \
         --principal "*" \
         --query "Statement" \
